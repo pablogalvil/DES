563364084a90790bf39b80adea9536ae77e58ff5
pablogalvil <pablogalvil53@gmail.com>
2024-09-26 14:55:12 +0200

    septiembre 26

--- unidad1/ejemplo_variables.php
+++ unidad1/ejemplo_variables.php
@@ -1,17 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <?php
+
+    define("MAX_HEALTH", 100);
+
+    const MIN_ALUMNOS = 5;
+
+    $i = 12;
     $num1 = 23;
     $num2 = "24";
     $num3 = $num1 + $num2;
+    $num4 = 24;
+    $array_numeros = array(12, 45, 92, 27, 9, 2, 7);
 
     print "La suma de $num1 y $num2 es $num3<br>";
+
+    print PHP_INT_MAX . "<br>";
+    if (is_int($num2))
+        print "Es entero<br>";
+    else
+        print "No es entero<br>";
 
     $num1 = "veintitres";
 
@@ -19,7 +35,7 @@
 
     print $num1;
 
-    if(2>=2){
+    if (2 >= 2) {
         //nombre esta declarado dentro del if solo accesible dentro del if
         $nombre = "pepe";
         print "<br> Entra";
@@ -31,7 +47,7 @@
     echo "<br> El nombre es $nombre <br>";
 
     //strlen nos dice cuantos caracteres tiene un string
-    print "El nombre tiene ".strlen($nombre)." caracteres <br>";
+    print "El nombre tiene " . strlen($nombre) . " caracteres <br>";
 
     var_dump($nombre);
 
@@ -40,20 +56,37 @@
     $empleado = array("pedro", 34, 2345.4, "soltero");
 
     //count nos cuenta la cantidad de elementos que tiene un array
-    print "<br>El array empleado tiene ".count($empleado)." elementos<br>";
+    print "<br>El array empleado tiene " . count($empleado) . " elementos<br>";
 
     var_dump($empleado);
 
     print "<br> $texto <br>";
 
     //str_replace solo lo devuelve, no modifica la variable original
-    print (str_replace("daw", "smr", $texto));
+    print(str_replace("daw", "smr", $texto));
 
     print "<br> $texto <br>";
-    
+
     //exploode corta una cadena y devuelve un array de secciones de dicha cadena
     //Hay que indicarle el caracter que los separa.
     var_dump(explode(" ", $texto));
+
+    print "<br>" . min($array_numeros) . "<br>";
+
+    print round(4.50) . "<br>";
+
+    print "Numero aleatorio: " . rand(0.0, 3.50);
+
+    print "<br>La vida maxima es: " . MAX_HEALTH;
+
+    if ($num2 === $num4)
+        print "<br>Son iguales";
+    else
+        print "<br>No son iguales";
+
+    $valor = ($num2 + $num2) / 2;
+
     ?>
 </body>
+
 </html>
