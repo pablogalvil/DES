71b9879cf4a8e66f9d0cf50a7acc97fa004a5830
pablogalvil <pablogalvil53@gmail.com>
2024-10-22 14:56:36 +0200

    octubre 22

--- unidad2/practicas/ejercicio3/ejercicio3.php
+++ unidad2/practicas/ejercicio3/ejercicio3.php
@@ -6,8 +6,52 @@
     <title>Document</title>
 </head>
 <body>
+    <style>
+        img{
+            width: 200px;
+        }
+    </style>
     <?php 
-    
+        //Genera la carta
+        $num_aleatorio = (int) rand(1, 13);
+        $tipo_aleatorio = (int) rand(0, 3);
+        $num_aleatorio2 = (int) rand(1, 13);
+        $tipo_aleatorio2 = (int) rand(0, 3);
+
+        //Array que guarda los tipos de las cartas.
+        $tipos = ["clubs", "diamonds", "hearts", "spades"];
+
+        //Mostramos las cartas usando img y poniendo la ruta de cada carta.
+        echo 'Tus cartas son : <br> <img src="img/'.$num_aleatorio.'_of_'.$tipos[$tipo_aleatorio].'.png"> 
+        <img src="img/'.$num_aleatorio2.'_of_'.$tipos[$tipo_aleatorio2].'.png">';
+
+        //Cambio el valor para que salga 1 como la mayor
+        if ($num_aleatorio == 1){
+            $num_aleatorio = 14;
+        }
+
+        if ($num_aleatorio2 == 1){
+            $num_aleatorio2 = 14;
+        }
+
+        //Condicional para ver cual es mayor, o si son iguales.
+        if ($num_aleatorio == $num_aleatorio2){
+            echo "<br>Las dos cartas tienen un valor similar";
+        } else if ($num_aleatorio > $num_aleatorio2){
+            //Condicion para mostrar 1 debido al cambio anterior
+            if ($num_aleatorio == 14){
+                echo "<br>La carta 1 es la mayor";
+            }else{
+                echo "<br>La carta ".$num_aleatorio." es la mayor";
+            }
+        } else{
+            //Condicion para mostrar 1 debido al cambio anterior
+            if ($num_aleatorio2 == 14){
+                echo "<br>La carta 1 es la mayor";
+            }else{
+                echo "<br>La carta ".$num_aleatorio2." es la mayor";
+            }
+        }
     ?>
 </body>
 </html>
---
+++ unidad2/practicas/ejercicio4/ejercicio4.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        $moneda1_user1 = (int) rand(1, 2);
+        $moneda2_user1 = (int) rand(1, 2);
+
+        $moneda1_user2 = (int) rand(1, 2);
+        $moneda2_user2 = (int) rand(1, 2);
+
+        if ($moneda1_user1 == 1 && $moneda2_user1 == 1 && ($moneda1_user2 != 1 || $moneda2_user2 != 1)){
+            echo ("El ganador es el jugador 1");
+        }
+    ?>
+</body>
+</html>
