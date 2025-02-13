dde202bb41c88a61252068c69e694b3cf63610ee
Pablo Galan <pablogalvil53@gmail.com>
2025-02-13 14:58:39 +0100

    febrero 13

---
+++ laravel/app/Models/Cliente.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cliente extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'nombre',
+        'email',
+        'telefono',
+        'direccion'
+    ];
+
+    public function mascotas(){
+        return $this->hasMany(Mascota::class);
+    }
+
+    public function ventas(){
+        return $this->hasMany(Venta::class);
+    }
+}
---
+++ laravel/database/migrations/2025_02_13_125259_create_mascotas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mascotas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('especie');
+            $table->integer('edad');
+            $table->decimal('precio', 8, 2);
+            $table->foreignId('cliente_id')->nullable()->constrained('clientes')->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mascotas');
+    }
+};
